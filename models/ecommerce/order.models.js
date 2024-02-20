acfcdcc2bbfa8d2745725daa5d29928514497c42
Subrata Barick <subrata.barick27@gmail.com>
2024-02-20 15:38:37 +0530

    third commit

---
+++ models/ecommerce/order.models.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+
+const orderitemsSchema=new mongoose.Schema({
+    ptoductId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Product"
+    },
+    quantity:{
+        type:Number,
+        required:true
+    }
+})
+
+
+const orderSchema=new mongoose.Schema({
+    orderprice:{
+        type:Number,
+        required:true
+
+    },
+    customer:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
+    orderItems:{
+        type:[orderitemsSchema]
+    },
+    address:{
+        type:String,
+        required:true
+    },
+    status:{
+        type:String,
+        enum:["PENDING","CANCELED","DELIVERED"],
+        default:"PENDING"
+
+    }
+
+},{timestamps:true})
+export const Order=mongoose.model("Order",orderSchema)
